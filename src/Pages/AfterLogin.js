000ccf8c178f9054be122a05aa75e6a2f24e323b
Pranay Sharma <pranaysharma431@gmail.com>
2024-05-30 18:53:22 +0530

    ev

--- src/Pages/AfterLogin.js
+++ src/Pages/AfterLogin.js
@@ -4,15 +4,15 @@ import propic from '../assests/propic.jpg'
 const AfterLogin = () => {
   return (
     <div className='dash'><div id='dashh'>Welcome To Your Dashboard</div>
-   <div><div id='dashf'>    <div id='picdash'> <div>User Name :</div><img src={propic} id='propic'/></div></div>
+   <div><div id='dashf'>  <div>Firebase login <trsh>youtubeupload</trsh></div>  <div id='picdash'> <div>User Name :</div><img src={propic} id='propic'/></div></div>
     
     <div id='dashf2'>
-    <div> Email Id</div>
-    <div>Number</div>
-    <div>jnfjnfnf</div>
-    <div>fjntkjgy</div>
-    <div>tlnghjbfyk</div>
-    <div>ljnvyjknk</div>
+    <div> Email Id :</div>
+    <div>Number :</div>
+    <div>jnfjnfnf :</div>
+    <div>fjntkjgy :</div>
+    <div>tlnghjbfyk :</div>
+    <div>ljnvyjknk :</div>
     </div>
     
 
